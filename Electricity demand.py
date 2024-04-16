7ee33bed5c1d9bc438eec69e406a7f84587a762b
Armitahoo <ahooman@syr.edu>
2024-04-16 15:26:16 -0400

    Update Electricity demand.py

--- Electricity demand.py
+++ Electricity demand.py
@@ -16,12 +16,11 @@ egypt_data.dropna(inplace=True)
 # Create pivot table for the specified columns
 new_pivot = egypt_data.pivot_table(values=['electricity_demand', 'electricity_generation', 'electricity_share_energy', 'per_capita_electricity'], index='year', aggfunc='sum')
 
-# Calculate electricity demand per capita
-egypt_data['electricity_demand_per_capita'] = egypt_data['electricity_demand'] / egypt_data['population']
+
 
 # Plot the electricity demand per capita
 plt.figure(figsize=(12, 6), dpi=300)
-plt.plot(egypt_data['year'], egypt_data['electricity_demand_per_capita'], marker='o', color='blue', linewidth=2)
+plt.plot(egypt_data['year'], egypt_data['per_capita_electricity'], marker='o', color='blue', linewidth=2)
 plt.title('Electricity Demand per Capita in Egypt (2000-2022)')
 plt.xlabel('Year')
 plt.ylabel('Electricity Demand per Capita (kWh)')
