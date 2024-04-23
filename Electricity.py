0b98b56948fca3e519a4eeaf0bd98e3427fc7ea7
Armitahoo <ahooman@syr.edu>
2024-04-23 17:36:19 -0400

    Update Electricity.py

--- Electricity.py
+++ Electricity.py
@@ -40,30 +40,3 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 
-
-
-
-plt.xlabel('Type')
-plt.ylabel('Energy Production (TJ)')
-plt.title('Domestic Electricity Generation in Egypt (2021)')
-
-plt.xticks(rotation=45, ha='right')
-plt.tight_layout()
-plt.show()
-
-
-# Read the CSV file for per capita electricity source
-df_per_capita = pd.read_csv('C:/Users/user/OneDrive/Documents/Advanced Willy/Final Data/Presentation/solaregypt/per-capita-electricity-source-stacked.csv')
-
-# Filter data for Egypt
-df_per_capita_egypt = df_per_capita[df_per_capita['Entity'] == 'Egypt']
-
-# Plot the bar chart for per capita electricity source
-plt.figure(figsize=(12, 6), dpi=300)
-plt.bar(df_per_capita_egypt['Year'], df_per_capita_egypt.iloc[:, 2:].sum(axis=1), color='skyblue')
-
-plt.xlabel('Year')
-plt.ylabel('Per Capita Electricity (kWh)')
-plt.title('Per Capita Electricity Source in Egypt')
-plt.tight_layout()
-plt.show()
